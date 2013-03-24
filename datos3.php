59438c259bad7fef76442d39f1ef9b11929a004a
Pablo Durantez <pablodurcas@gmail.com>
2013-03-24 12:57:16 +0100

    cambios6

--- datos3.php
+++ datos3.php
@@ -2,22 +2,13 @@
 
 $token = 'AAAHvfgA7ueMBABqhp67ECryTZAMhebAjsZAGw05i3u2hfqjTBmkQPWstzdhwqehmQ0EZCXLOFiFM23BdqNDnRZBqZBvHmThBTbu5xaBKWyAZDZD';
 $msg = 'Se ha inscrito a la liga de Buscando Padel';
-$title = 'prueba de titulo';
-/*
+$title = 'Super Liga BuscandoPadel';
 $uri= 'www.buscandopadel.com';
-$desc = 'prueba de descripción';
-*/
+$desc = 'La red social del padel';
 $pic = 'http://www.buscandopadel.com/images/avatar/groups/d5736bc8ee930f4c430fc8c0.jpg';
 
 
-$attachment =  array(
-'access_token' => $token,
-'message' => $msg,
-'name' => $title,
-'picture'=>$pic
-);
 
-/*
 $attachment =  array(
 'access_token' => $token,
 'message' => $msg,
@@ -26,7 +17,8 @@ $attachment =  array(
 'description' => $desc,
 'picture'=>$pic
 );
-*/
+
+
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,'https://graph.facebook.com/me/feed');
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
