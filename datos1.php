ab294a5f4bdd235e65a1a61ba6bb42c492859d54
Pablo Durantez <pablodurcas@gmail.com>
2013-03-24 11:41:31 +0100

    cambios4

--- datos1.php
+++ datos1.php
@@ -1,7 +1,7 @@
 <?php
 
 $graph_url = "https://graph.facebook.com/me?access_token=" 
-       . 'access_token';
+       . 'AAAHvfgA7ueMBABqhp67ECryTZAMhebAjsZAGw05i3u2hfqjTBmkQPWstzdhwqehmQ0EZCXLOFiFM23BdqNDnRZBqZBvHmThBTbu5xaBKWyAZDZD';
 
      $data = json_decode(file_get_contents($graph_url));
      print_r ($data);
